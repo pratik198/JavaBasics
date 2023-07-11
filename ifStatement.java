286ebe2ffca910095eb07132872d4cbda2264f45
Arun Pratik Rout <130307910+pratik198@users.noreply.github.com>
2023-07-11 13:37:02 +0530

    Add files via upload

---
+++ ifStatement.java
@@ -0,0 +1,68 @@
+class ifStatement {
+    public static void main(String[] args) {
+        /*
+         * ===if else statement===
+         * 
+         *          
+         */
+                //find out if the person pass or fail
+        //  int math =10;
+         
+        //  if(math >30 ) {
+        //     System.out.println("pass");
+        //  }else {
+        //     System.out.println("fail");
+        //  }
+
+                    //CONDITIONAL STATEMENT//
+         /*
+          *   less than = a<b
+              less or equal = a<=b
+              greater than = a>b
+              greater than or equal = a>=b
+              equal to a==b
+              not equal a!=b
+
+          */
+
+          /*
+           * 1=Use "if" to specify a block of code to be executed,if a specified codition is true.
+           * 2= else is use to specify a block of code to be executed if the same condition is false.
+           * 3= else if  is use to specify  a new condition to test,is the first condition is true.
+           * 
+           *        THE IF STATEMENT==
+           *    Syntax ====
+           *                if (condition) {
+            *       // block of code to be executed if the condition is true
+
+                    THE ELSE STATEMENT==
+                Syntax === 
+                            if(condition) {
+                                if the condition is true the block of code will exexute
+                            } else {
+                                if first condition is false then it will execute else statement 
+                            }
+                    THE ELSE IF STATEMENT === 
+                Syntax === if (conditon) {
+                                block of code to be executed if condition is true
+                            }else if(condition) {
+                                block of code to be executed if the first condition is false and second 
+                                condition is true 
+                            }else {
+                                block of code will execute if both conditons are false.
+                            }
+                            
+
+           * }
+           * 
+           */
+          int a = 39;
+          int b = 60;
+
+            if(a>b){
+                System.out.println("a is greater than b");
+            }else{
+                System.out.println("islessthan");
+            }
+    }
+}
